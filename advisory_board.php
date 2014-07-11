68653e50c36eac99b756ede315b84fb91cc146c9
Aram Zucker-Scharff <azuckers@gmu.edu>
2014-07-11 16:20:24 -0400

    finish php switch over

---
+++ advisory_board.php
@@ -0,0 +1,50 @@
+<?php 
+
+require_once('head.php');
+?>
+	<title>Balfour Investors - Advisory Board </title>
+	<meta name="description" content="Balfour Investors' founders are veteran distressed investors who have made significant investments in troubled companies during a multitude of economic cycles and industry downturns. With 90 years of aggregate experience they have invested their own capital as lead investors in joint ventures with their partners in a broad range of transactions exceeding $1 billion dollars in value."/>
+	<meta name="keywords"
+	content="Balfour Investors, Financial Advisors, Distressed Investors, Troubled Companies, Investments, Industry Downturns, Economic Cycles, Advisory Work"/>
+</head>
+<?php
+require_once('header.php');
+?>
+
+<div id="content" align="center">
+<div id="center_content_noscroll" align="justify">
+<table>
+<tr>
+<td valign="top"  align="left" width="20%">
+<img src="images/home_left.jpg" alt=""/>
+</td>
+<td valign="top" align="justify">
+<a name="top"></a>
+<img src="images/advisory_board.jpg" alt=""/>
+<br/>
+<br/>
+<table cellpadding="8" cellspacing="8" border="0">
+<tr>
+<!--<td>
+<a href="Keri_Aivazis.php"><em>Keri Aivazis</em></a><br/>
+<strong>Advisor</strong>
+</td>-->
+<td>
+<a href="Donald_Cecil.php"><em>Donald Cecil</em></a><br/>
+<strong>Advisor</strong>
+</td>
+<td>
+<a href="Jeffrey_Hecktman.php"><em>Jeffrey B. Hecktman</em></a><br/>
+<strong>Advisor</strong>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</div>
+</div>
+<?php 
+require_once('footer.php');
+
+?>
---
+++ bios.php
@@ -0,0 +1,50 @@
+<?php 
+
+require_once('head.php');
+?>
+	<title>Balfour Investors - Bios </title>
+	<meta name="description" content="Balfour Investors' founders are veteran distressed investors who have made significant investments in troubled companies during a multitude of economic cycles and industry downturns. With 90 years of aggregate experience they have invested their own capital as lead investors in joint ventures with their partners in a broad range of transactions exceeding $1 billion dollars in value."/>
+	<meta name="keywords"
+	content="Balfour Investors, Financial Advisors, Distressed Investors, Troubled Companies, Investments, Industry Downturns, Economic Cycles, Advisory Work"/>
+</head>
+<?php
+require_once('header.php');
+?>
+
+<div id="content" align="center">
+<div id="center_content_noscroll" align="justify">
+<table width="100%">
+<tr>
+<td valign="top" align="left" width="25%">
+<img src="images/home_left.jpg" alt=""/>
+</td>
+<td valign="top" align="justify">
+<a name="top"></a>
+<img src="images/bios.jpg" alt=""/>
+<br/>
+<br/>
+<table cellpadding="8" cellspacing="8" border="0">
+<tr>
+<td>
+<a href="Harry_Freund_Jay_Goldsmith.php"><em>Harry Freund</em></a><br/>
+<strong>Chairman</strong>
+</td>
+<td>
+<a href="Harry_Freund_Jay_Goldsmith.php"><em>Jay Goldsmith</em></a><br/>
+<strong>President</strong>
+</td>
+<td>
+<a href="Marc_Sherman.php"><em>Marc S. Sherman</em></a><br/>
+<strong>Director</strong>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</div>
+</div>
+<?php 
+require_once('footer.php');
+
+?>
