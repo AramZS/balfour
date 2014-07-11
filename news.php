355cbb5c2cb310e341ba56d417f5d3a9780f069e
Aram Zucker-Scharff <azuckers@gmu.edu>
2014-07-11 15:46:53 -0400

    Here's the news

--- news.php
+++ news.php
@@ -13,32 +13,29 @@ require_once('header.php');
 
 <div id="content" align="center">
 	<div id="center_content_noscroll" align="justify">
-		<table valign="top">
-			<tr>
-				<td valign="top" rowspan="4" colspan="1">
-					<img src="images/home_left.jpg" alt=""/>
-				</td>
-				<td valign="top" colspan="4" rowspan="1" align="justify" height="180px">
-				<p>Balfour Investors is a merchant bank whose principals have acquired assets across a multitude of industries for over 35 years. It is the private investment office of Harry Freund and Jay Goldsmith.
+		<div id="newsbox" class="inner">
+			<h2>
+				News
+			</h2>
+			<div class="article">
+				<h3>
+					SEACOR Holdings Inc. and Balfour Investors Inc. Announce Formation of CLEANCOR Energy Solutions LLC
+				</h3>
+				<p>
+				SEACOR Holdings Inc. and Balfour Investors Inc. today announced the formation of CLEANCOR Energy Solutions LLC ("CLEANCOR"), an energy merchant bank focusing on financing for and investment in clean-fuel start-up or early stage enterprises, technologies, infrastructure, logistics services, and projects.
 				</p>
-				<p>Over the years, particularly during times of unstable economic cycles and industry downturns, they have made significant investments in both troubled and performing companies.  Balfour's principals have also engaged in specialized advisory work.</p>
-					<p>
-					Balfour Investors has partnered with SEACOR Holdings Inc. to create CLEANCOR, an energy merchent bank focused on financing for and investment in clean-fuel start-up or early stage enterprises, technologies, infrastructyure, logistics services and projects. 
-					</p>
-				</td>
-			</tr>
-			<tr valign="top" height="30px">
-				<th colspan="4" valign="top">Latest news:</th>
-			</tr>
-			<tr valign="top">
-				<td colspan="1" width="90px">Date</td>
-				<td colspan="3">Item</td>
-			</tr>
-			<tr valign="top">
-				<td><strong>8/20/2013:</strong></td>
-				<td>Balfour Investors and SEACOR Holdings announces formation of CLEANCOR.</td>
-			</tr>
-		</table>
+				<p><a href="article-seacor-balfour-announce.php">Read More</a></p>
+			</div>
+			<div class="article">
+				<h3>
+					SEACOR Holdings Inc. and Balfour Investors Inc. Announce Formation of CLEANCOR Energy Solutions LLC
+				</h3>
+				<p>
+				SEACOR Holdings Inc. and Balfour Investors Inc. today announced the formation of CLEANCOR Energy Solutions LLC ("CLEANCOR"), an energy merchant bank focusing on financing for and investment in clean-fuel start-up or early stage enterprises, technologies, infrastructure, logistics services, and projects.
+				</p>
+				<p><a href="article-seacor-balfour-announce.php">Read More</a></p>
+			</div>			
+		</div>
 	</div>
 </div>
 
