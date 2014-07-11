1d0d3b462fcf6173cfa20d42b30d42b7f469f4c8
Aram Zucker-Scharff <azuckers@gmu.edu>
2014-07-11 15:46:44 -0400

    Flipping over to easier PHP

--- footer.php
+++ footer.php
@@ -3,12 +3,12 @@
 <div id="gray_bottom" align="center">
 <img src="images/bottom_nav.jpg" alt=""/>
 <ul>
-<li><a href="index.html">Home</a></li>
-<li><a href="bios.html">Bios</a></li>
-<li><a href="investment_strategy.html">Investment Strategy</a></li>
-<li><a href="portfolio_companies.html">Portfolio Companies</a></li>
-<li><a href="advisory_board.html">Advisory Board</a></li>
-<li><a href="contact_info.html">Contact Info</a></li>
+<li><a href="index.php">Home</a></li>
+<li><a href="bios.php">Bios</a></li>
+<li><a href="investment_strategy.php">Investment Strategy</a></li>
+<li><a href="portfolio_companies.php">Portfolio Companies</a></li>
+<li><a href="advisory_board.php">Advisory Board</a></li>
+<li><a href="contact_info.php">Contact Info</a></li>
 </ul>
 <span class="credits">
 &copy; Balfour Investors, Inc.
--- header.php
+++ header.php
@@ -7,15 +7,15 @@
 <table width="100%" id="nav" cellpadding="0" cellspacing="0" border="0">
 <tr>
 <td width="33%" valign="top">
-<a href="index.html" onmouseover="document.home.src='images/nav_home.jpg'"
+<a href="index.php" onmouseover="document.home.src='images/nav_home.jpg'"
 onmouseout="document.home.src='images/nav_home_hover.jpg'">
 <img src="images/nav_home_hover.jpg" alt="Home" name="home"/>
 </a>
-</td><td width="33%" align="center" valign="top"><a href="bios.html" onmouseover="document.bios.src='images/nav_bios.jpg'"
+</td><td width="33%" align="center" valign="top"><a href="bios.php" onmouseover="document.bios.src='images/nav_bios.jpg'"
 onmouseout="document.bios.src='images/nav_bios_hover.jpg'">
 <img src="images/nav_bios_hover.jpg" alt="Bios" name="bios"/>
 </a></td><td width="33%" valign="top" align="right">
-<a href="investment_strategy.html" onmouseover="document.invest.src='images/nav_investment.jpg'"
+<a href="investment_strategy.php" onmouseover="document.invest.src='images/nav_investment.jpg'"
 onmouseout="document.invest.src='images/nav_investment_hover.jpg'">
 <img src="images/nav_investment_hover.jpg" alt="Investment Strategy" name="invest"/>
 </a>
@@ -24,15 +24,15 @@ onmouseout="document.invest.src='images/nav_investment_hover.jpg'">
 </table>
 <img src="images/header_img.jpg" alt="Balfour Partners Investment Strategies"/>
 <ul id="bottom_nav">
-<li><a href="portfolio_companies.html" onmouseover="document.portfolio.src='images/nav_portfolio.jpg'"
+<li><a href="portfolio_companies.php" onmouseover="document.portfolio.src='images/nav_portfolio.jpg'"
 onmouseout="document.portfolio.src='images/nav_portfolio_hover.jpg'">
 <img src="images/nav_portfolio_hover.jpg" alt="Portfolio Companies" name="portfolio" />
 </a></li>
-<li><a href="advisory_board.html" onmouseover="document.advise.src='images/nav_advisory.jpg'"
+<li><a href="advisory_board.php" onmouseover="document.advise.src='images/nav_advisory.jpg'"
 onmouseout="document.advise.src='images/nav_advisory_hover.jpg'">
 <img src="images/nav_advisory_hover.jpg" alt="Advisory Board" name="advise" />
 </a></li>
-<li><a href="contact_info.html" onmouseover="document.contact.src='images/nav_contact.jpg'"
+<li><a href="contact_info.php" onmouseover="document.contact.src='images/nav_contact.jpg'"
 onmouseout="document.contact.src='images/nav_contact_hover.jpg'">
 <img src="images/nav_contact_hover.jpg" alt="Contact Info" name="contact" /></a></li>
 </ul>
