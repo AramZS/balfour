f59fa9834b39efa5422a2ce6075e0193689d6cb4
Aram Zucker-Scharff <azuckers@gmu.edu>
2014-07-11 16:40:42 -0400

    Sample article 2

---
+++ article-two.php
@@ -0,0 +1,40 @@
+<?php 
+require_once('head.php');
+?>
+	<title>Balfour Investors </title>
+	<meta name="description" content="Balfour Investors' founders are veteran distressed investors who have made significant investments in troubled companies during a multitude of economic cycles and industry downturns. With 90 years of aggregate experience they have invested their own capital as lead investors in joint ventures with their partners in a broad range of transactions exceeding $1 billion dollars in value."/>
+	<meta name="keywords"
+	content="Balfour Investors, Financial Advisors, Distressed Investors, Troubled Companies, Investments, Industry Downturns, Economic Cycles, Advisory Work"/>
+</head>
+<?php
+require_once('header.php');
+?>
+
+<div id="content" align="center">
+	<div id="center_content_noscroll" align="justify">
+		<div id="article-box" class="inner">
+			<div class="article">
+				<h2>
+					SEACOR Holdings Inc. and Balfour Investors Inc. Announce Formation of CLEANCOR Energy Solutions LLC
+				</h2>
+				<div class="featured-image">
+					<img src="http://www.seacorholdings.com/img/SeacorDavis.png" width="460" height="253" alt="SEACOR Davis">
+				</div>
+				<p>
+					FORT LAUDERDALE, FL--(Marketwired - Aug 20, 2013) - SEACOR Holdings Inc. and Balfour Investors Inc. today announced the formation of CLEANCOR Energy Solutions LLC ("CLEANCOR"), an energy merchant bank focusing on financing for and investment in clean-fuel start-up or early stage enterprises, technologies, infrastructure, logistics services, and projects.
+				</p>
+					<p>CLEANCOR will be a full service solution provider, initially concentrating on investments in the end use of clean fuel such as conversion of diesel engines to alternative fuel engines, the manufacturing of and installations for key components found in liquefaction, production, and storage that are required for developing markets, and delivering clean fuel to end users.
+					</p>
+					<h4>About SEACOR Holdings</h4>
+					<p>SEACOR is a global provider of marine transportation equipment and logistics services primarily servicing the U.S. and international energy and agricultural markets. SEACOR offers customers a diversified suite of services and equipment, including offshore marine, inland river, storage and handling, distribution of petroleum, chemical and agricultural commodities, and shipping. SEACOR is dedicated to building innovative, modern, "next generation," efficient marine equipment while providing highly responsive service with the highest safety standards, and dedicated professional employees. SEACOR is publicly traded on the New York Stock Exchange (NYSE) under the symbol CKH.
+</p>					
+					<h4>About Balfour Investors</h4>
+					<p>Balfour Investors is a merchant bank whose principals have acquired assets across a multitude of industries for over 35 years. It is the private investment office of Harry Freund and Jay Goldsmith. Over the years, particularly during times of unstable economic cycles and industry downturns, they have made significant investments in both troubled and performing companies. Balfour's principals have also engaged in specialized advisory work</p>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php 
+require_once('footer.php');
